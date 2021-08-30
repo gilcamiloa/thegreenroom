8dc750f1b6625d7534331d519df93dee6728d039
SimonBao <Simonbao.sb@gmail.com>
2021-08-30 17:16:42 +0100

    create message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,17 @@
 class MessagesController < ApplicationController
   def new
-    @messages = Message.new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.date = Time.now
+    @message.save!
+  end
+
+  private
+
+  def message_params 
+    params.require(:messages).permit(:chatroom, :user, :content)
   end
 end
