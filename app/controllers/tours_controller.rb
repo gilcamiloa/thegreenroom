2262553a56f211a20dd5bf488bfe9f7604fcde0e
Camilo Gil <cgilv6@gmail.com>
2021-08-30 17:43:50 +0100

    created ToursController

---
+++ app/controllers/tours_controller.rb
@@ -0,0 +1,41 @@
+class ToursController < ApplicationController
+  before_action :set_tour, only: %i[new create show index]
+
+  def index
+    if params[:user_id]
+      @tours = User.find(params[:user_id]).tours
+    else
+      @tours = Tours.all.order(start_time: :DESC)
+    end
+  end
+
+  def new
+    @tour = Tour.new
+    @booking = Booking.find(params(:booking_id))
+  end
+
+  def create
+    @tour = Tour.new(tour_params)
+    @booking = Booking.find(params[:booking_id])
+    @tour.booking = @booking
+    @tour.start_time = @booking.date
+    @booking.user = current_user
+    if @tour.save
+      redirect_to tours_path
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  private
+
+  def tour_params
+    params.require(:tour).permit(:name, :start_time)
+  end
+
+  def set_tour
+    @tour = Tour.find(params[:id])
+  end
+end
