893a2a42637b07843409ea3a49920974ef243083
Katherine Wise <k06kw02@gmail.com>
2021-08-30 17:21:29 +0100

    created the chatroom controller

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,19 @@
+class ChatroomController < ApplicationController
+  def new
+    @chatroom = Chatroom.new()
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+  end
+
+  def index
+    @chatroom = Chatroom.find(params[:user_id])
+  end
+end
+
+private
+
+def chatroom_params
+  params.require(:chatroom).permit(:name, :venue, :user)
+end
