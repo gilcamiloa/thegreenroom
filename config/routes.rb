29de31bea3f60871a70f249bc5502d529ba5c169
Katherine Wise <k06kw02@gmail.com>
2021-08-30 16:26:21 +0100

    made the controllers, tours is provisional

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :venues, only [:index, :show] do
+    resources :bookings, only [:new, :index]
+    resources :reviews, only [:new]
+  end
+
+  resources :users, only [:index] do
+    resources :chatrooms, only [:index]
+  end
+
+  resources :chatrooms, only [:new, :show] do
+    resources :messages, only [:new]
+  end
+
+  resources :tours, only [:new, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
