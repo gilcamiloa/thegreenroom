3e85cedb5ecf46a1e0d22afd1ac63ff2c8bb1891
SimonBao <Simonbao.sb@gmail.com>
2021-08-31 11:39:43 +0100

    Create review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :booking
+  validates :user, presence: true
+  validates :booking, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: 1..5, presence: true
+  validates :content, presence: true
 end
