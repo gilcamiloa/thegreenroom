dbc5a70cf79a3f844a3ee77862d7fc64c01e7b94
SimonBao <Simonbao.sb@gmail.com>
2021-08-31 11:11:49 +0100

    Create booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :venue
   belongs_to :user
   belongs_to :tour, optional: true
+  validates :venue, presence: true
+  validates :user, presence: true
+  validates :dates, presence: true
+  validates :status, presence: true, inclusion: [true, false]
 end
