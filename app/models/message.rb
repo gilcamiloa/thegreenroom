531ca5c290d4b15aabebbdb81b331f75524fd93b
SimonBao <Simonbao.sb@gmail.com>
2021-08-31 10:52:14 +0100

    Create user validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :user
   validate :date, presence: true
-  validate :conten, presence: true
+  validate :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :venues, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :chatrooms, dependent: :destroy
+  has_many :tours, dependent: :destroy
+  validates :is_band, type: boolean, presence: true
+  validates :username, presence: true
+  validates :band_name, presence: true
+  validates :band_description, presence: true
 end
