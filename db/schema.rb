18f4b268f9c3081ec5c4ee64bdc1fbe06c2ec8be
James Wolfe <jr.wolfe@outlook.com>
2021-08-31 17:25:07 +0100

    homepage layout

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 2021_08_30_142752) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_band"
     t.string "band_name"
-    t.string "username"
     t.text "band_description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
