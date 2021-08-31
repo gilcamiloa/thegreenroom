945ec8c8d4a39ecef6a3f8fa6a07738bc686bceb
SimonBao <Simonbao.sb@gmail.com>
2021-08-31 11:17:26 +0100

    Create chatroom validations

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,7 @@
 class Chatroom < ApplicationRecord
   belongs_to :venue
   belongs_to :user
+  validates :user, prescence: true
+  validates :venue, prescence: true
+  validates :name, prescence: true
 end
