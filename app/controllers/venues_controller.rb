737e1a7c10f2ce65dd6147896ad36d162f6ccc32
SimonBao <Simonbao.sb@gmail.com>
2021-09-01 14:14:56 +0100

    create calendar with only available dates clickable

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -7,7 +7,8 @@ class VenuesController < ApplicationController
 
   def show
     @venue = Venue.find(params[:id])
-    @dates = @venue.available_dates.to_json
-    # @dates = ['2021-08-27T06:00:00', '2021-08-26T06:00:00', '2021-08-29T06:00:00', '2021-08-30T06:00:00', '2021-08-31T06:00:00']
+    @dates = @venue.available_dates.map { |date| 
+      Date.parse(date.to_s).strftime("%Y-%m-%d")
+    }
   end
 end
--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -8,59 +8,59 @@ export default class extends Controller {
 
   connect() {
     const dates = JSON.parse(this.data.get('available_dates'));
-    document.addEventListener('turbolinks:load', () => {
-      const myEvents = dates.map(date => ({
-        start: date,
-        end: date
-       })) 
-
-      let selectedDate = {};
-      const onClick = (e) => {
-        let day;
-        let dateSplit = String(e).split(' ').slice(0,4);
-        let date = new Date(dateSplit);
-        day = (dateSplit[2][0] === "0" ? dateSplit[2][1] : dateSplit);
-        if (date in selectedDate) {
-          selectedDate[date].selected = !selectedDate[date].selected;
-        } else {
-          selectedDate[date] = { selected: true, day: day };
-        }
-        let selectedDateElement= document.querySelectorAll('.calendar__day.calendar__day-active.calendar__day-event');
-        for (const [key, value] of Object.entries(selectedDate)) {
-          console.log(`${key}: ${value.selected}, day: ${value.day}`);
-          for (const element of selectedDateElement) { 
-            if (element.innerText === value.day && value.selected) {
-              element.classList.add('calendar__day-selected-date');
-            } else {
-              element.classList.remove('calendar__day-selected-date');
-            }
-          }
-        }
-      }
-
-      new Calendar({
-        id: '#color-calendar',
-        eventsData: myEvents,
-        selectedDateClicked: onClick,
-        disableMonthArrowClick: true,
-        dateChanged: (date, e) => {
-        }
-      })
-
-      let calendar_dates= document.querySelectorAll('.calendar__day.calendar__day-active');
-      calendar_dates.forEach(date => {
-        if (date.classList.contains('calendar__day-event')) { 
-          return;
-        } else if (date.classList.contains('calendar__day-selected')) {
-          date.classList.remove('calendar__day-selected');
-          date.classList.remove('calendar__day-today');
-          date.classList.remove('calendar__day-no-event');
-        }
-        date.style.pointerEvents = "none";
-        date.classList.add('calendar__day-other');
-        date.innerHTML = date.innerText;
-      });
-    });
+//     document.addEventListener('turbolinks:load', () => {
+//       const myEvents = dates.map(date => ({
+//         start: date,
+//         end: date
+//        })) 
+// 
+//       let selectedDate = {};
+//       const onClick = (e) => {
+//         let day;
+//         let dateSplit = String(e).split(' ').slice(0,4);
+//         let date = new Date(dateSplit);
+//         day = (dateSplit[2][0] === "0" ? dateSplit[2][1] : dateSplit);
+//         if (date in selectedDate) {
+//           selectedDate[date].selected = !selectedDate[date].selected;
+//         } else {
+//           selectedDate[date] = { selected: true, day: day };
+//         }
+//         let selectedDateElement= document.querySelectorAll('.calendar__day.calendar__day-active.calendar__day-event');
+//         for (const [key, value] of Object.entries(selectedDate)) {
+//           console.log(`${key}: ${value.selected}, day: ${value.day}`);
+//           for (const element of selectedDateElement) { 
+//             if (element.innerText === value.day && value.selected) {
+//               element.classList.add('calendar__day-selected-date');
+//             } else {
+//               element.classList.remove('calendar__day-selected-date');
+//             }
+//           }
+//         }
+//       }
+// 
+//       new Calendar({
+//         id: '#color-calendar',
+//         eventsData: myEvents,
+//         selectedDateClicked: onClick,
+//         disableMonthArrowClick: true,
+//         dateChanged: (date, e) => {
+//         }
+//       })
+// 
+//       let calendar_dates= document.querySelectorAll('.calendar__day.calendar__day-active');
+//       calendar_dates.forEach(date => {
+//         if (date.classList.contains('calendar__day-event')) { 
+//           return;
+//         } else if (date.classList.contains('calendar__day-selected')) {
+//           date.classList.remove('calendar__day-selected');
+//           date.classList.remove('calendar__day-today');
+//           date.classList.remove('calendar__day-no-event');
+//         }
+//         date.style.pointerEvents = "none";
+//         date.classList.add('calendar__day-other');
+//         date.innerHTML = date.innerText;
+//       });
+//     });
 
   }
 }
