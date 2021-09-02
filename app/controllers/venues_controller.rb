cdc583b82a06dd8a490891fb2011b5a7b9864dea
James Wolfe <jr.wolfe@outlook.com>
2021-09-03 00:19:16 +0100

    implemented cloudinary - no test

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,7 @@
 require 'json'
 class VenuesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @venues = Venue.all
     @markers = @venues.geocoded.map do |venue|
@@ -11,11 +12,32 @@ class VenuesController < ApplicationController
       }
     end
   end
+
   def show
     @venue = Venue.find(params[:id])
     @booking = Booking.new
-    @available_dates = @venue.available_dates.map { |date| 
+    @available_dates = @venue.available_dates.map { |date|
       Date.parse(date.to_s).strftime("%Y-%m-%d")
     }
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.user = current_user
+    if @venue.save!
+      redirect_to venue_path(@venue)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :address, :description, :capacity, :available_dates, photos: [])
   end
 end
