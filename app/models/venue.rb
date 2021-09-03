376b453b76ffb8894c1cb7f64f63b48617cc134a
James Wolfe <jr.wolfe@outlook.com>
2021-09-03 11:12:01 +0100

    fixed geocoding for venues/new

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -5,10 +5,8 @@ class Venue < ApplicationRecord
   validates :name, presence: true
   validates :capacity, presence: true
   validates :address, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
   validates :description, presence: true
   accepts_nested_attributes_for :bookings
-  after_validation :geocode, if: :will_save_change_to_address?
   geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
