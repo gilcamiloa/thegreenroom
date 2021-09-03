79c8fcd5803f370d18eb3c1f1827095a3689870f
SimonBao <Simonbao.sb@gmail.com>
2021-09-03 12:15:51 +0100

    updated styling for consistency and removed some weird ui

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,9 +5,10 @@ const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/k06kw02/ckszz90e17sx618o7dcya4qv9'
+    style: 'mapbox://styles/k06kw02/ckszz90e17sx618o7dcya4qv9',
   });
 };
+// center: [52.6358333, -1.1390972]
 // style: 'mapbox://styles/mapbox/streets-v11'
 
 const addMarkersToMap = (map, markers) => {
@@ -29,7 +30,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 10 });
 };
 
 const initMapbox = () => {
