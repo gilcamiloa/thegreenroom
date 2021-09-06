ad9257f34c35ffca3b035e62687728788a3d80f4
SimonBao <Simonbao.sb@gmail.com>
2021-09-06 12:51:18 +0100

    When date cannot be double booked

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,6 +14,14 @@ class BookingsController < ApplicationController
   end
 
   def create
+    create_booking
+    update_venue
+    save_booking
+  end
+
+  private
+
+  def create_booking
     @booking = Booking.new
     @booking.dates = params[:booking][:dates].split(',').map do |date|
       Date.parse(date).to_datetime
@@ -24,6 +32,16 @@ class BookingsController < ApplicationController
     @booking.tour = @tour
     @booking.user = current_user
     @booking.status = false
+  end
+
+  def update_venue
+    @booking.dates.each do |date|
+      @booking.venue.available_dates.delete(date)
+    end
+    redirect_to venue_path(@venue) unless @booking.venue.update({ available_dates: @booking.venue.available_dates })
+  end
+
+  def save_booking
     if @booking.save!
       # placeholder path untill we have a bookings page
       redirect_to tour_path(@tour)
