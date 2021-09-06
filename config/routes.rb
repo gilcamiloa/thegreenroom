b046aae063da57e7ce0ed3b333ee16d2fc9164ac
SimonBao <Simonbao.sb@gmail.com>
2021-09-06 10:39:03 +0100

    create venues booking index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :venues, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [ :new, :create ]
+    resources :bookings, only: [ :new, :create, :index, :show ]
     resources :reviews, only: [:new]
   end
 
