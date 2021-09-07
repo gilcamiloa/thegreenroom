40604f461164f76a7495eb60d1efb491f6ee0dd6
SimonBao <Simonbao.sb@gmail.com>
2021-09-07 16:39:08 +0100

    generate youtube/soundcloud playlist from user input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,13 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
+    @updated_params = user_params.clone
+    generate_playlist_format
+    if @user.update(@updated_params)
       redirect_to user_path(@user)
     else
       render :edit
@@ -23,7 +24,18 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :band_description, :photo)
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :band_description, :photo, :youtube_playlist,
+                                 :soundcloud_playlist)
           .delete_if { |_key, value| value.blank? }
+  end
+
+  def generate_playlist_format
+    if user_params[:youtube_playlist] && @user.youtube_playlist != @updated_params[:youtube_playlist]
+      @updated_params[:youtube_playlist] = @updated_params[:youtube_playlist].match(/(?<=list=).*/)[0]
+    end
+    if user_params[:soundcloud_playlist] && @user.soundcloud_playlist != @updated_params[:soundcloud_playlist]
+      @updated_params[:soundcloud_playlist] = @updated_params[:soundcloud_playlist].match(/(?<=playlists\/).*(?=&color)/)[0]
+    end
   end
 end
